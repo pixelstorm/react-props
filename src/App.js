62e20aecce546013ddc3763266bea1d4e5c9f475
pixelstorm <dan@pixelstorm.com.au>
2021-07-26 06:58:03 +1000

    fix key prop id console error

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ const employeeInfo = [
     firstName: "jack",
     lastName: "hilo",
     age: "22",
+    id: "sksskdj",
   },
   {
     firstName: "jiiick",
     lastName: "hilsllo",
     age: "22",
+    id: "sskdkdj",
   },
   {
     firstName: "jsssck",
     lastName: "hijjjjo",
     age: "22",
+    id: "skdj",
   },
 ];
 function App() {
   return (
     <div className="App">
       {employeeInfo.map((employee) => {
-        return <Employee {...employee} />;
+        return <Employee key={employee.id} {...employee} />;
       })}
     </div>
   );
