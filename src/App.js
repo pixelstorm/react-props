6bb04150453ac1bd47c807dc38986e22bc826418
pixelstorm <dan@pixelstorm.com.au>
2021-07-25 16:23:31 +1000

    output dynamic name value for each employee

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import Employee from "./Employee";
 function App() {
   return (
     <div className="App">
-      <Employee />;
+      <Employee name="Jack" />;
+      <Employee name="Brian" />;
+      <Employee name="George" />;
+      <Employee name="harry" />;
     </div>
   );
 }
--- src/Employee.js
+++ src/Employee.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Employee = () => {
-  return <h1>this is an employee</h1>;
+const Employee = (props) => {
+  return <h1>this is an {props.name}</h1>;
 };
 
 export default Employee;
