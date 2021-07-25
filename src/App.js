9f9fef7c0f244886655a860f43521477fc16b068
pixelstorm <dan@pixelstorm.com.au>
2021-07-26 06:51:24 +1000

    simplify code with spread operator

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ function App() {
   return (
     <div className="App">
       {employeeInfo.map((employee) => {
-        const { firstName, lastName, age } = employee;
-        return <Employee firstName={firstName} lastName={lastName} age={age} />;
+        return <Employee {...employee} />;
       })}
     </div>
   );
